2e3820f1c040785a4724e1a5f10d4fe16be2daeb
Benoit Sarels <benoit.sarels@sorbonne-universite.fr>
2021-02-05 13:48:24 +0100

    STRANGcore.py: - Passage de la taille 3 à une taille N quelconque - Enregistrement du front incident et usage de InterpolatedUnivariateSpline pour l'extrapoler à tout l'espace reac-diff-STRANG-nvector.py: - Renommage de u, v, w en y1, y2, y3 - Adaptation à la nouvelle sortie de evolve - Ajout de kink, energie, gradenergie et usage de curve_fit pour fitter les fronts - Ajout du code pour deux nouvelles figures:   * Allele frequencies at time t   * Instantaneous wave speed - Suppression de la ligne pour enregistrer l'animation en mp4 - Ajout de la ligne pour enregistrer les données en npy

--- STRANGcore.py
+++ STRANGcore.py
@@ -2,7 +2,7 @@
 solve a vector reaction-diffusion equation:
 
 phi_t = kappa phi_{xx} + frhs(phi)
-where phi = [u, v, w]
+where phi = [u1, u2, ..., uk, ..., uN]
 
 using operator splitting, with implicit diffusion
 
@@ -13,50 +13,57 @@ B. Sarels based on code from M. Zingale
 
 import numpy as np
 from scipy.integrate import ode
+from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.linalg import solve_banded
 
 class Grid(object):
 
-    def __init__(self, nx, xmin, xmax, ng = 1, vars = None):
+    def __init__(self, nx, systemsize, xmin, xmax, ng = 1, vars = None):
         """ grid class initialization """
 
-        self.nx = nx
-        self.ng = ng
+        self.nx = nx	# number of interior points
+        self.systemsize = systemsize
+        self.ng = ng	# number of points to handle a boundary, 1 in 1D
 
         self.xmin = xmin
         self.xmax = xmax
 
         self.dx = (xmax - xmin) / (nx + ng)
-        self.x  = (np.arange(ng + nx + ng)) * self.dx + xmin
+        self.x  = np.arange(ng + nx + ng) * self.dx + xmin
 
         self.ilo = ng - 1
         self.ihi = ng + nx
 
-        self.data = {}
+        self.data = {}	# data est un dictionnaire
 
         for v in vars:
-            self.data[v] = np.zeros((3*(ng + nx + ng)), dtype = np.float64)
+            self.data[v] = np.zeros((systemsize*(ng + nx + ng)), dtype = np.float64)
 
     def scratch_array(self):
-        return np.zeros((3*(self.ihi + self.ng)), dtype = np.float64)
+        return np.zeros((self.systemsize*(self.ihi + self.ng)), dtype = np.float64)
 
     def initialize(self):
         """ initial condition """
 
         phi = self.data["phi"]
-        phi[0*self.ihi + 0*self.ng : 1*self.ihi + 1*self.ng] = (self.x < 50.)
-        phi[1*self.ihi + 1*self.ng : 2*self.ihi + 2*self.ng] = 0.
-        phi[2*self.ihi + 2*self.ng : 3*self.ihi + 3*self.ng] = 0.
+# provide an initial condition such that y1>=0, y2>=0, y3>=0 and y1+y2+y3<=1
+        SA = 0.1
+        SB = 0.1
+        p = 1./(1.+np.exp(np.sqrt(SA)*(self.x-15.)))
+        q = 1./(1.+np.exp(np.sqrt(SB)*(self.x-45.)))
+        phi[0 * (self.ihi + self.ng) : 1 * (self.ihi + self.ng)] = p     *q
+        phi[1 * (self.ihi + self.ng) : 2 * (self.ihi + self.ng)] = p     *(1.-q)
+        phi[2 * (self.ihi + self.ng) : 3 * (self.ihi + self.ng)] = (1.-p)*q
 
-def react(gr, phi, frhs, dt):
+def react(gr, phi, systemsize, frhs, dt):
     """ react phi through timestep dt """
 
     phinew = gr.scratch_array()
 
-    for i in range(gr.ilo, gr.ihi + 1):
+    for i in range(gr.ilo, gr.ihi + gr.ng):
 #        r = ode(frhs, jac)
         r = ode(frhs)
-        phi0 = [phi[0*gr.ihi + 0*gr.ng + i], phi[1*gr.ihi + 1*gr.ng + i], phi[2*gr.ihi + 2*gr.ng + i]]
+        phi0 = phi[i : : (gr.ihi + gr.ng)]
         t0 = 0.
         r.set_initial_value(phi0, t0)
 # “vode”: real-valued Variable-coefficient Ordinary Differential Equation solver,
@@ -65,101 +72,149 @@ def react(gr, phi, frhs, dt):
 # - implicit Adams method (for non-stiff problems) (method = "adams")
 # - a method based on backward differentiation formulas (for stiff problems) (method = "bdf")
         r.set_integrator("vode", method = "adams")
-        r.set_f_params()
+        r.set_f_params(gr.x[i])
         r.integrate(r.t + dt)
-        phinew[0*gr.ihi + 0*gr.ng + i] = r.y[0]
-        phinew[1*gr.ihi + 1*gr.ng + i] = r.y[1]
-        phinew[2*gr.ihi + 2*gr.ng + i] = r.y[2]
+        for k in range(0, systemsize):
+            phinew[k * (gr.ihi + gr.ng) + i] = r.y[k]
 
     return phinew
 
-def diffuse(gr, phi, kappa, dt):
+def diffuse(gr, phi, systemsize, kappa, dt):
     """ diffuse phi implicitly (Crank - Nicolson) through timestep dt """
 
     phinew = gr.scratch_array()
 
     alpha = kappa*dt/gr.dx**2
 
-    # create the RHS of the matrix
-    u = phi[0*gr.ihi + 0*gr.ng : 1*gr.ihi + 1*gr.ng]
-    v = phi[1*gr.ihi + 1*gr.ng : 2*gr.ihi + 2*gr.ng]
-    w = phi[2*gr.ihi + 2*gr.ng : 3*gr.ihi + 3*gr.ng]
+    u = np.zeros((systemsize, (gr.ihi + gr.ng)))
+    R = np.zeros((systemsize, (gr.ihi + gr.ng)))
     diag     = -2.0 * np.eye(gr.ihi + gr.ng)
     surdiag  = np.eye(gr.ihi + gr.ng, k = 1)
     sousdiag = np.eye(gr.ihi + gr.ng, k = -1)
     A  = diag + surdiag + sousdiag
+    # set the boundary conditions by changing the matrix elements
+    # homogeneous Neumann BC
     A[0, 1] = 2.0
     A[-1, -2] = 2.0
     I  = np.eye(gr.ihi + gr.ng)
-    TR = I + alpha / 2.0 * A # Tridiagonal matrix in the Right hand side
-    R1 = TR @ u
-    R2 = TR @ v
-    R3 = TR @ w
-    #TL = I - alpha / 2.0 * A # Tridiagonal matrix in the Left hand side
-    # create the diagonal, upper and lower parts of the matrix
-    D = (1.0 + alpha) * np.ones(gr.ihi + gr.ng)
-    U = -0.5 * alpha  * np.ones(gr.ihi + gr.ng)
-    L = -0.5 * alpha  * np.ones(gr.ihi + gr.ng)
-    U[0]  = 0.0 # Valeur de toute façon inutile pour solve_banded
-    L[-1] = 0.0 # Valeur de toute façon inutile pour solve_banded
-    # set the boundary conditions by changing the matrix elements
-    # homogeneous Neumann BC
-    U[1]  = -1.0*alpha
-    L[-2] = -1.0*alpha
-    BL = np.matrix([U, D, L]) # Banded matrix in the Left hand side
 
-    # solve
-    phinew[0*gr.ihi + 0*gr.ng : 1*gr.ihi + 1*gr.ng] = solve_banded((1,1), BL, R1)
-    phinew[1*gr.ihi + 1*gr.ng : 2*gr.ihi + 2*gr.ng] = solve_banded((1,1), BL, R2)
-    phinew[2*gr.ihi + 2*gr.ng : 3*gr.ihi + 3*gr.ng] = solve_banded((1,1), BL, R3)
+    for k in range(0, systemsize):
+        # 1. define right hand side
+        u[k, : ] = phi[k * (gr.ihi + gr.ng) : (k+1) * (gr.ihi + gr.ng)]
+        TR = I + alpha[k] / 2.0 * A # Tridiagonal matrix in the right hand side
+        R[k, : ] = TR @ u[k, : ]
+
+        # 2. define left hand side
+        #TL = I - alpha[k] / 2.0 * A # Tridiagonal matrix in the left hand side
+        # create the diagonal, upper and lower parts of the matrix
+        D = (1.0 + alpha[k]) * np.ones(gr.ihi + gr.ng)
+        U = -0.5 * alpha[k]  * np.ones(gr.ihi + gr.ng)
+        L = -0.5 * alpha[k]  * np.ones(gr.ihi + gr.ng)
+        U[0]  = 0.0 # Valeur de toute façon inutile pour solve_banded
+        L[-1] = 0.0 # Valeur de toute façon inutile pour solve_banded
+        # set the boundary conditions by changing the matrix elements
+        # homogeneous Neumann BC
+        U[1]  = -1.0*alpha[k]
+        L[-2] = -1.0*alpha[k]
+        BL = np.matrix([U, D, L]) # Banded matrix in the left hand side
+
+        # 3. solve
+        phinew[k * (gr.ihi + gr.ng) : (k+1) * (gr.ihi + gr.ng)] = solve_banded((1,1), BL, R[k, : ])
+
     return phinew
 
 def est_dt(gr, kappa, sigma = 1.):
     """ estimate the timestep """
 # In the old context of solving phi_t = kappa phi_{xx} + sigma R(phi)
-# the speed is proportional to the square root of kappa * sigma
-    s = np.sqrt(kappa * sigma)
+# the speed is proportional to the square root of (kappa * sigma)
+    kappamin = kappa[kappa > 0.].min()
+    s  = np.sqrt(kappamin * sigma)
     dt = gr.dx/s
 
     return dt
 
-def evolve(nx, xmin, xmax, tmin, tmax, kappa, frhs):
+def evolve(nx, systemsize, xmin, xmax, tmin, tmax, kappa, frhs):
     """
     the main evolution loop.  Evolve
 
-     phi_t = kappa phi_{xx} + frhs(phi)
+    phi_t = kappa phi_{xx} + frhs(phi)
 
     from t = tmin to tmax
     """
 
     # create the grid
-    gr = Grid(nx, xmin, xmax, vars = ["phi", "phi1", "phi2"])
+    gr = Grid(nx, systemsize, xmin, xmax, vars = ["phi", "phi1", "phi2"])
 
     # create the time vector
-    dt = est_dt(gr, kappa)
-    T  = np.arange(tmin, tmax, dt)
+    dt = est_dt(gr, kappa, 2.)
+    T  = np.arange(tmin, tmax, dt) # note that more often than not, tmax is not a point in T
 
     # pointers to the data at various stages
     phi  = gr.data["phi"]
     phi1 = gr.data["phi1"]
     phi2 = gr.data["phi2"]
 
-    sol  = np.zeros((T.size, 3*(gr.ihi + gr.ng)), dtype = np.float64)
+    sol  = np.zeros((T.size, systemsize*(gr.ihi + gr.ng)), dtype = np.float64)
 
     # initialize
     gr.initialize()
     sol[0] = phi
 
     for n in range(1, T.size):
-
         # react for dt/2
-        phi1[:] = react(gr, phi, frhs, dt/2)
+        phi1[:] = react(gr, phi, systemsize, frhs, dt/2)
         # diffuse for dt
-        phi2[:] = diffuse(gr, phi1, kappa, dt)
+        phi2[:] = diffuse(gr, phi1, systemsize, kappa, dt)
         # react for dt/2 -- this is the updated solution
-        phi[:] = react(gr, phi2, frhs, dt/2)
+        phi[:] = react(gr, phi2, systemsize, frhs, dt/2)
 
+        # debug
+        y1 = phi[0 * (gr.ihi + gr.ng) : 1 * (gr.ihi + gr.ng)]
+        y2 = phi[1 * (gr.ihi + gr.ng) : 2 * (gr.ihi + gr.ng)]
+#        print (T[n], y1.max(), y2.max())
+#        print (n, T[n])
+
+        # incident front at t = 5
+        if n == int(5/dt):
+            # record incident front
+            y1ref = phi[0 * (gr.ihi + gr.ng) : 1 * (gr.ihi + gr.ng)]
+            # shift incident front
+            i = 0
+            while y1ref[i] > 0.5:
+                i += 1
+            imilieu = i
+            # shift incident front - autre méthode
+            sorter = np.arange(y1ref.size)[::-1]
+            imilieu2 = y1ref.size - np.searchsorted(y1ref, 0.5, sorter = sorter)
+            imilieu3 = y1ref.size - np.searchsorted(y1ref[::-1], 0.5)
+            x0pourx1ref = imilieu*gr.dx + xmin
+            x1ref = gr.x - x0pourx1ref
+#            print (n, T[n])
+#            print (y1ref)
+#            print ('imilieu', imilieu, y1ref[imilieu-1], y1ref[imilieu])
+#            print ('imilieu2', imilieu2)
+#            print ('imilieu3', imilieu3)
+#            print (x0pourx1ref)
+#            print (x1ref)
+            # positions to inter/extrapolate
+            x = np.linspace(-100., 100., 5000)
+            # spline order: 1 linear, 2 quadratic, 3 cubic ... 
+            order = 1
+            # do inter/extrapolation
+            s = InterpolatedUnivariateSpline(x1ref, y1ref, k = order)
+            y = s(x)
+
+            # example showing the interpolation for linear, quadratic and cubic interpolation
+#            plt.figure()
+#            plt.grid(True)
+#            plt.plot(x1ref, y1ref, linestyle=':')
+#            for order in range(1, 4):
+#                s = InterpolatedUnivariateSpline(x1ref, y1ref, k = order)
+#                y = s(x)
+#                plt.plot(x, y)
+#            plt.show()
+
+        # write to sol
         sol[n] = phi
 
-    else:
-        return sol, gr.x, T, gr.ng
+    return sol, T, gr
--- reac-diff-STRANG-nvector.py
+++ reac-diff-STRANG-nvector.py
@@ -2,7 +2,7 @@
 solve a vector reaction-diffusion equation:
 
 phi_t = kappa phi_{xx} + frhs(phi)
-where phi = [u, v, w]
+where phi = [y1, y2, y3]
 
 using operator splitting, with implicit diffusion
 
@@ -12,84 +12,195 @@ B. Sarels
 """
 
 import numpy as np
-from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+#import time
 
 from STRANGcore import evolve
 
 # Parameters for diffusion
-kappa = 0.025
+kappa = np.array([1., 1., 1.])
 
 # Parameters for reaction
-# They should be put in the function itself
-def frhs(t, phi):
+sA = 0.
+SA = 0.1
+cp = sA/np.sqrt(SA)
+sB = 0.
+SB = 0.1
+rAB = 0.1
+# Should they be put in the function itself?
+def frhs(t, phi, x):
     """ reaction ODE righthand side """
 
-    u, v, w = phi
-    D  = u * ( 1. - u - v - w ) - v * w
+    y1, y2, y3 = phi
+    y4 = 1. - y1 - y2 - y3
+    D  = y1 * y4 - y2 * y3
 
-    S  = 0.05
-    sB = 0.05
-    r0 = 0.0001
+    dy1dt  = -2.*(SA       *(y1+y2)**2. + SB        *(y1+y3)**2.)
+    dy1dt +=     (3.*SA-sA)*(y1+y2)     + (3.*SB-sB)*(y1+y3)
+    dy1dt += sA - SA + sB - SB
+    dy1dt  = y1 * dy1dt
+    dy1dt += - rAB * D
 
-    dudt = u * ( - S + S * ( u + v ) * ( 3. - 2. * ( u + v ) ) + sB * ( u + w - 1. ) ) - r0 * D
-    dvdt = v * ( - S + S * ( u + v ) * ( 3. - 2. * ( u + v ) ) + sB * ( u + w ) ) + r0 * D
-    dwdt = w * ( S * ( u + v ) * ( 1. - 2. * ( u + v ) ) + sB * ( u + w - 1. ) ) + r0 * D
+    dy2dt  = -2.*(SA       *(y1+y2)**2. + SB        *(y1+y3)**2.)
+    dy2dt +=     (3.*SA-sA)*(y1+y2)     + (SB   -sB)*(y1+y3)
+    dy2dt += sA - SA
+    dy2dt  = y2 * dy2dt
+    dy2dt += rAB * D
 
-    return [dudt, dvdt, dwdt]
+    dy3dt  = -2.*(SA       *(y1+y2)**2. + SB        *(y1+y3)**2.)
+    dy3dt +=     (SA   -sA)*(y1+y2)     + (3.*SB-sB)*(y1+y3)
+    dy3dt += sB - SB
+    dy3dt  = y3 * dy3dt
+    dy3dt += rAB * D
+
+    return np.array([dy1dt, dy2dt, dy3dt])
 
 # Parameters for the problem
-nx   = 198
+nx   = 299
 xmin = 0.
 xmax = 100.
 tmin = 0.
-tmax = 400.
+tmax = 300.
 
-phi, x, T, ng = evolve(nx, xmin, xmax, tmin, tmax, kappa, frhs)
+phi, T, grille = evolve(nx, 3, xmin, xmax, tmin, tmax, kappa, frhs)
 
-u = phi[ : , 0*nx + 0*ng + 0 : 1*nx + 1*ng + 1]
-v = phi[ : , 1*nx + 1*ng + 1 : 2*nx + 2*ng + 2]
-w = phi[ : , 2*nx + 2*ng + 2 : 3*nx + 3*ng + 3]
-z = 1. - u - v - w # attention aux notations z est la 4eme variable
-p = w + z
-q = v + z
-D = u * z - v * w
-dx = (xmax - xmin)/nx # mettre ça ailleurs : dans la sortie de evolve ?
-dpdx = np.gradient(p, dx)
-dqdx = np.gradient(q, dx)
+y1 = phi[ : , 0*phi.shape[1]//3 : 1*phi.shape[1]//3 ]
+y2 = phi[ : , 1*phi.shape[1]//3 : 2*phi.shape[1]//3 ]
+y3 = phi[ : , 2*phi.shape[1]//3 : ]
+y4 = 1. - y1 - y2 - y3
+p = y1 + y2
+q = y1 + y3
+D = y1 * y4 - y2 * y3
+#dpdx = np.gradient(p, grille.dx)
+#dqdx = np.gradient(q, grille.dx)
+
+def kink(x, centre):
+    S = 0.1
+    return 1./(1.+np.exp(np.sqrt(S)*(x-centre)))
+
+def energie(centre, x, y, S):
+    erreur = 0.
+    for i in range(0, y.size):
+        erreur += (y[i] - 1./(1.+np.exp(np.sqrt(S)*(x[i]-centre))))**2.
+    return erreur/y.size
+
+def gradenergie(centre, x, y, S):
+    grad = 0.
+    for i in range(0, y.size):
+        grad += - (y[i] - 1./(1.+np.exp(np.sqrt(S)*(x[i]-centre)))) * np.sqrt(S) * \
+        np.exp(np.sqrt(S)*(x[i]-centre)) * (1.+np.exp(np.sqrt(S)*(x[i]-centre)))**-2.
+    return 2.*grad/y.size
+
+centres = np.zeros((phi.shape[0], 2))
+for n in range(phi.shape[0]): # fit
+    # initial guess
+    centrep = centres[n-1, 0]
+    centreq = centres[n-1, 1]
+    # call to curve_fit
+    poptp, pcovp = curve_fit(kink, grille.x, p[n , : ], p0 = centrep)
+    poptq, pcovq = curve_fit(kink, grille.x, q[n , : ], p0 = centreq)
+    # write to centres
+    centres[n, 0] = poptp
+    centres[n, 1] = poptq
+
+dcentrespdt = np.gradient(centres[ : , 0], T[1]-T[0])
+dcentresqdt = np.gradient(centres[ : , 1], T[1]-T[0])
 
 # Parameters for the visualisation
-ymin = 0.
-ymax = 1.1
+petit = 0.05
+ymin0 = p.min() - petit
+ymax0 = p.max() + petit
+ymin1 = dcentresqdt[2 : ].min() - petit
+ymax1 = dcentrespdt[2 : ].max() + petit
 
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax  = plt.axes(xlim = (xmin, xmax), ylim = (ymin, ymax))
-linep, = ax.plot([], [], lw = 2)
-lineq, = ax.plot([], [], lw = 2)
-lineD, = ax.plot([], [], lw = 2)
-time_text  = ax.text(0.02, 0.95, '', transform = ax.transAxes)
+# 1. create a figure with several subplots
+figure1, axes = plt.subplots(1, 2, dpi = 80, figsize = (16, 12), sharex = 'col') # faire attention aux dpi si les figures sont pour latex
 
-def indice():
-    """ indice function. It will give me a nice iterable """
-    for i in range(T.size):
-        yield i
+# 2. initialize the line objects (one in each axes)
+line1p,  = axes[0].plot([], [], lw = 2, label = 'p')
+line1q,  = axes[0].plot([], [], lw = 2, label = 'q')
+line1D,  = axes[0].plot([], [], lw = 2, label = 'D')
+line1vp, = axes[1].plot([], [], lw = 2, label = 'speed of p')
+color1p  = line1vp.get_color()
+axes[1].hlines(cp, tmin, tmax, lw = 1, colors = color1p, linestyles = 'dashed')
+line1vq, = axes[1].plot([], [], lw = 2, label = 'speed of q')
+
+# 3. axes initializations
+for ax in axes:
+    ax.grid()
+axes[0].set_xlim(xmin, xmax)
+axes[1].set_xlim(tmin, tmax)
+axes[0].set_ylim(ymin0, ymax0)
+axes[1].set_ylim(ymin1, ymax1)
+axes[0].set_xlabel('space')
+axes[1].set_xlabel('time')
+axes[0].legend(handles = [line1p, line1q, line1D])
+axes[1].legend(handles = [line1vp, line1vq])
 
 def animate(i):
-    """ animate function. It is called following the indice iterable """
-    linep.set_data(x, p[i, : ])
-    lineq.set_data(x, q[i, : ])
-    lineD.set_data(x, D[i, : ])
-    time_text.set_text('time = %.1f' % T[i])
-    ax.set_title('time = %.1f' % T[i])
-#    return linep, lineq, lineD, time_text
+    """ animate function """
+    line1p.set_data (grille.x, p[i, : ])
+    line1q.set_data (grille.x, q[i, : ])
+    line1D.set_data (grille.x, D[i, : ])
+    line1vp.set_data(T[2 : i], dcentrespdt[2 : i])
+    line1vq.set_data(T[2 : i], dcentresqdt[2 : i])
+    figure1.suptitle('time = %.1f' % T[i])
+    axes[0].set_title('p, q, D at time = %.1f' % T[i])
+    axes[1].set_title('speed of p and q until time = %.1f' % T[i])
 
-# call the animator. 'blit = True' means only re-draw the parts that have changed.
-# but pay attention! If using 'blit = True', then animate has to have a return statement and then I cannot have the title to change...
-courbe = animation.FuncAnimation(fig, animate, indice, interval = 200)
+# call animation
+courbe = animation.FuncAnimation(figure1, animate, frames = T.size, repeat = False)
+figure1.show()
 
-# save the animation as a gif/mp4
-#courbe.save('genet_pop.gif', fps = 30, writer = 'imagemagick')
-#courbe.save('genet_pop.mp4', fps = 30, extra_args = ['-vcodec', 'libx264'])
 
-plt.show()
+
+#figure2 = plt.figure(figsize = (12, 8), dpi = 80)
+#for n in range(0, phi.shape[0], 3):
+#    plt.clf()
+#    plt.title('Allele frequencies at time $t = {:3.1f}$'.format(T[n]))
+#    plt.grid(True)
+#    plt.xlim(xmin, xmax)
+#    plt.ylim(ymin0, ymax0)
+#    plt.xlabel('Space')
+#    plt.ylabel('')
+#    plt.plot(grille.x, p[n, : ], label = 'p')
+#    plt.plot(grille.x, q[n, : ], label = 'q')
+#    plt.plot(grille.x, D[n, : ], label = 'D')
+#    plt.legend(loc = 'best')
+#    figure2.show()
+#    plt.pause(0.1)
+#    FileNameTimestepN = 'sA'+str(sA)+'SA'+str(SA)+'sB'+str(sB)+'SB'+str(SB)+'rAB'+str(rAB)+'timestep'+str(n)+'.png'
+##    plt.savefig('data/'+FileNameTimestepN, bbox_inches='tight')
+#
+#figure3 = plt.figure(figsize = (12, 8), dpi = 80)
+#plt.title('Instantaneous wave speed')
+#line3vp, = plt.plot(T[2 : ], dcentrespdt[2 : ], label = 'speed of p')
+#color3p = line3vp.get_color()
+#plt.hlines(cp, tmin, tmax, lw = 1, colors = color3p, linestyles = 'dashed')
+#plt.plot(T[2 : ], dcentresqdt[2 : ], label = 'speed of q')
+#plt.grid(True)
+#plt.xlim(tmin, tmax)
+#plt.ylim(ymin1, ymax1)
+#plt.xlabel('Time')
+#plt.ylabel('Wave speed')
+#plt.legend(loc = 'best')
+#figure3.show()
+#FileNameSpeeds = 'sA'+str(sA)+'SA'+str(SA)+'sB'+str(sB)+'SB'+str(SB)+'rAB'+str(rAB)+'speeds.png'
+#plt.savefig('data/'+FileNameSpeeds, bbox_inches='tight')
+#
+#
+#
+#FileNameGlobal = 'sA'+str(sA)+'SA'+str(SA)+'sB'+str(sB)+'SB'+str(SB)+'rAB'+str(rAB)
+#
+# save the animation as a gif file
+#print ('debut de sauvegarde')
+#start1 = time.time()
+#courbe.save('data/'+FileNameGlobal+'.gif', dpi = 80, writer = 'imagemagick')
+#print ('temps de sauvegarde', time.time() - start1)
+#print ('fin de sauvegarde')
+#
+# save the data as an npy file
+#np.save('data/'+FileNameGlobal+'.npy', phi)
+
